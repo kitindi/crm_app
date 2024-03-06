397560cd9a89ec3f8e8a3793e6021d7adfa4cfb5
kitindi <abdullkitindi@gmail.com>
2024-03-06 11:08:18 +0300

    initial commit

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'accounts/dashboard.html')
+
+
+def products(request):
+    return render(request, 'accounts/products.html')
+def customer(request):
+    return render(request, 'accounts/customer.html')
